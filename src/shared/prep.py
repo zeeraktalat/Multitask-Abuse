f568e47ef26a259d68246dd7094d989a7d3bfafa
Zeerak Waseem <zeerakw@laptop>
2019-10-14 16:01:04 +0100

    Move type imports to types and remove batch size requirement from initialisaiton

--- src/shared/prep.py
+++ src/shared/prep.py
@@ -1,8 +1,7 @@
 import re
 import spacy
 from torchtext import data
-import src.shared.types as types
-from typing import List, Tuple, Dict, Union, Callable, Any
+import src.shared.types as t
 
 
 class BatchGenerator:
@@ -23,17 +22,16 @@ class BatchGenerator:
 
 class Dataset(data.TabularDataset):
 
-    def __init__(self, data_dir: str, splits: Dict[str, str], ftype: str,
-                 fields: List[Tuple[types.FieldType, ...]] = None, cleaners: List[str] = None,
-                 batch_sizes: Tuple[int, ...] = (32), shuffle: bool = True, sep: str = 'tab', skip_header: bool = True,
-                 repeat_in_batches = False, device: str = 'cpu'):
+    def __init__(self, data_dir: str, splits: t.Dict[str, str], ftype: str,
+                 fields: t.List[t.Tuple[t.FieldType, ...]] = None, cleaners: t.List[str] = None,
+                 shuffle: bool = True, sep: str = 'tab', skip_header: bool = True, repeat_in_batches = False,
+                 device: str = 'cpu'):
         """Initialise data class.
         :param data_dir (str): Directory containing dataset.
-        :param fields (Dict[str, str]): The data fields in the file.
-        :param cleaners List[str]: Cleaning operations to be taken.
-        :param splits (str): Dictionary containing filenames type of data.
+        :param fields (t.Dict[str, str]): The data fields in the file.
+        :param cleaners t.List[str]: Cleaning operations to be taken.
+        :param splits (str): t.Dictionary containing filenames type of data.
         :param ftype: File type of the data file.
-        :param batch_sizes (Tuple[int]): Tuple of batch size for each dataset.
         :param shuffle (bool, default: True): Shuffle the data between each epoch.
         :param sep (str): Seperator (if csv/tsv file).
         :param repeat_in_batches (bool, default: False): Repeat data within batches
@@ -49,10 +47,18 @@ class Dataset(data.TabularDataset):
                             'num_files': num_files}
         self.load_params.update(splits)
         self.dfields = fields
-        self.batch_sizes = batch_sizes
         self.repeat = repeat_in_batches
         self.cleaners = cleaners
         self.device = device
+
+    @property
+    def field_instances(self):
+        """Set or return the instances of the fields used."""
+        return self.field_types
+
+    @field_instances.setter
+    def field_instance(self, fields: t.Tuple[t.FieldType, ...]):
+        self.field_types = fields
 
     @property
     def fields(self):
@@ -71,7 +77,7 @@ class Dataset(data.TabularDataset):
     def data_params(self, params):
         self.load_params.update(params)
 
-    def load_data(self) -> Tuple[types.DataType, ...]:
+    def load_data(self) -> t.Tuple[t.DataType, ...]:
         """Load the dataset and return the data.
         :return data: Return loaded data.
         """
@@ -87,13 +93,13 @@ class Dataset(data.TabularDataset):
         return self.data
 
     @classmethod
-    def _data(cls, path: str, format: str, fields: Union[List[Tuple[types.FieldType, ...]], Dict[str, tuple]],
+    def _data(cls, path: str, format: str, fields: t.Union[t.List[t.Tuple[t.FieldType, ...]], t.Dict[str, tuple]],
               train: str, validation: str = None, test: str = None, skip_header: bool = True,
-              num_files: int = 3) -> Tuple[types.DataType, ...]:
+              num_files: int = 3) -> t.Tuple[t.DataType, ...]:
         """Use the loader in torchtext.
         :param path (str): Directory the data is stored in.
         :param format (str): Format of the data.
-        :param fields (Union[List[types.FieldType], Dict[str tuple]]): Initialised fields.
+        :param fields (t.Union[t.List[t.FieldType], t.Dict[str tuple]]): Initialised fields.
         :param train (str): Filename of the training data.
         :param validation (str, default: None): Filename of the development data.
         :param test (str, default: None): Filename of the test data.
@@ -105,25 +111,25 @@ class Dataset(data.TabularDataset):
                                               validation = validation, test = test, skip_header = skip_header)
         return splitted
 
-    def clean_document(self, text: types.DocType, processes: List[str] = None):
+    def clean_document(self, text: t.DocType, processes: t.List[str] = None):
         """Data cleaning method.
-        :param text (types.DocType): The document to be cleaned.
-        :param processes (List[str]): The cleaning processes to be undertaken.
+        :param text (t.DocType): The document to be cleaned.
+        :param processes (t.List[str]): The cleaning processes to be undertaken.
         :return cleaned: Return the cleaned text.
         """
         cleaned = str(text)
         if 'lower' in self.cleaners or 'lower' in processes:
             cleaned = cleaned.lower()
         if 'url' in self.cleaners or 'url' in processes:
-            cleaned = re.sub(r'https?:/\/\S+', 'URL', cleaned)
+            cleaned = re.sub(r'https?:/\/\S+', '<URL>', cleaned)
         if 'hashtag' in self.cleaners or 'hashtag' in processes:
-            cleaned = re.sub(r'#[a-zA-Z0-9]*\b', 'HASHTAG', cleaned)
+            cleaned = re.sub(r'#[a-zA-Z0-9]*\b', '<HASHTAG>', cleaned)
         if 'username' in self.cleaners or 'username' in processes:
-            cleaned = re.sub(r'@\S+', 'AT_USER', cleaned)
+            cleaned = re.sub(r'@\S+', '<USER>', cleaned)
 
         return cleaned
 
-    def tokenize(self, document: types.DocType, processes: List[str] = None):
+    def tokenize(self, document: t.DocType, processes: t.List[str] = None):
         """Tokenize the document using SpaCy and clean it as it is processed.
         :param document: Document to be parsed.
         :param processes: The cleaning processes to engage in.
@@ -135,28 +141,38 @@ class Dataset(data.TabularDataset):
             toks = [tok.text for tok in self.tagger(self.clean_document(document))]
         return toks
 
-    def generate_batches(self, sort_func: Callable, datasets: Tuple[types.DataType, ...] = None):
+    @property
+    def data_attr(self):
+        """Get or set the data attribute."""
+        return self.data
+
+    @data_attr.setter
+    def data_attr(self, data: t.Tuple[t.DataType, ...]):
+        if len(data) == 1:
+            self.data = (data[0], None, None)
+        elif len(data) == 2:
+            self.data = (data[0], None, data[1])
+        elif len(data) == 3:
+            self.data = data
+
+    def generate_batches(self, sort_func: t.Callable, datasets: t.Tuple[t.DataType, ...] = None,
+                         batch_sizes: t.Tuple[int, ...] = (32,)):
         """Create the minibatching here.
-        :param train (types.DataType, optional): Provide a processed train dataset.
-        :param test (types.DataType, optional): Provide a processed test dataset.
-        :param dev (types.DataType, optional): Provide a processed test dataset.
+        :param train (t.DataType, optional): Provide a processed train dataset.
+        :param test (t.DataType, optional): Provide a processed test dataset.
+        :param dev (t.DataType, optional): Provide a processed test dataset.
+        :param batch_size (t.Tuple[int, ...]): Define batch sizes.
         :return ret: Return the batched data.
         """
-        if datasets:
-            batches = data.BucketIterator.splits(datasets,
-                                                 self.batch_sizes,
-                                                 sort_key = sort_func,
-                                                 device = self.device,
-                                                 sort_within_batch = True, repeat = self.repeat)
-        else:
-            batches = data.BucketIterator.splits(self.data,
-                                                 self.batch_sizes,
-                                                 sort_key = sort_func,
-                                                 device = self.device,
-                                                 sort_within_batch = True, repeat = self.repeat)
+        self.data_attr = datasets
+        batches = data.BucketIterator.splits(self.data,
+                                             batch_sizes,
+                                             sort_key = sort_func,
+                                             device = self.device,
+                                             sort_within_batch = True, repeat = self.repeat)
         return batches
 
-    def tag(self, document: types.DocType):
+    def tag(self, document: t.DocType):
         """Tag document using spacy.
         :param document: Document to be parsed.
         :return doc: Document that has been passed through spacy's tagger.
@@ -164,11 +180,11 @@ class Dataset(data.TabularDataset):
         doc = self.tagger(self.clean_document(document))
         return doc
 
-    def get_spacy_annotations(self, document: types.DocType, processes: List[str]) -> Tuple:
+    def get_spacy_annotations(self, document: t.DocType, processes: t.List[str]) -> t.Tuple:
         """Get annotations from SpaCy requested.
         :param document: The document to process.
         :param processes: The annotation processes to get.
-        :return res (tuple): Tuple containing annotations requested.
+        :return res (tuple): t.Tuple containing annotations requested.
         """
         res = [(tok.text, tok.pos_, tok.dep_, (tok.dep_, tok.head.dep_)) for tok in document]
         token, pos, dep, head = zip(*res)
@@ -188,15 +204,15 @@ class Dataset(data.TabularDataset):
 
         return tuple(res)
 
-    def set_field_attribute(self, field: Union[types.FieldType, List[types.FieldType]],
-                            attribute: Union[str, List[str]],
-                            val: Union[Any, List[Any]]):
+    def set_field_attribute(self, field: t.Union[t.FieldType, t.List[t.FieldType]],
+                            attribute: t.Union[str, t.List[str]],
+                            val: t.Union[t.Any, t.List[t.Any]]):
         """Take an initialised field and an attribute.
-        :param field (types.FieldType): The field to be modified.
+        :param field (t.FieldType): The field to be modified.
         :param attribute (str): The attribute to modify.
-        :param val (types.AllBuiltin): The new value of the attribute.
+        :param val (t.AllBuiltin): The new value of the attribute.
         """
-        if isinstance(field, List):
+        if isinstance(field, t.List):
             for f, a, v in zip(field, attribute, val):
                 setattr(f, a, v)
         else:
--- src/shared/train.py
+++ src/shared/train.py
@@ -19,19 +19,61 @@ def sort_func(x):
     return len(x.data)
 
 
-def create_batches(data_dir, splits, ftype, fields, cleaners, batch_sizes, shuffle, sep, skip_header,
-                   repeat_in_batches, device):
+def store_fields(obj, data_field, label_field, **kwargs):
+    """Store fields in the dataset object. Final two fields always label, train.
+    :param data (t.FieldType): The field instance for the data.
+    :param label (t.FieldType): The field instance for the label.
+    :param kwargs: Will search for any fields in this.
+    """
+    if kwargs:
+        field = []
+        for k in kwargs:
+            if 'field' in k:
+                field.append(kwargs[k])
+    field.extend([label_field, data_field])
+    obj.field_instance = tuple(field)
 
+
+def create_batches(data_dir: str, splits: t.Dict[str, t.Union[str, None]], ftype: str, fields: t.Union[dict, list],
+                   cleaners: t.List[str], batch_sizes: t.Tuple[int, ...], shuffle: bool, sep: str, skip_header: bool,
+                   repeat_in_batches: bool, device: t.Union[str, int],
+                   data_field: t.Tuple[t.FieldType, t.Union[t.Dict, None]],
+                   label_field: t.Tuple[t.FieldType, t.Union[t.Dict, None]], **kwargs):
+
+    # Initiate the dataset object
     data = Dataset(data_dir = data_dir, splits = splits, ftype = ftype, fields = fields, cleaners = cleaners,
                    batch_sizes = batch_sizes, shuffle = shuffle, sep = sep, skip_header = skip_header,
                    repeat_in_batches = repeat_in_batches, device = device)
 
-    loaded = data.load_data()
-    if len(splits.keys()) == 1:
-        train, test = data.split(split_ratio = 0.8, stratified = True, strata_field = 'label')
+    # If the fields need new attributes set: set them.
+    # TODO assumes only data and field labels need modification.
+    if data_field[1]:
+        data.set_field_attribute(data_field[0], data_field[1]['attribute'], data_field[1]['value'])
+
+    if label_field[1]:
+        data.set_field_attribute(label_field[0], label_field[1]['attribute'], label_field[1]['value'])
+
+    # Store our Field instances so we can later access them.
+    store_fields(data, data_field, label_field, **kwargs)
+
+    data.fields = fields  # Update the fields in the class
+
+    loaded = data.load_data()  # Data paths exist in the class
+
+    if len([v for v in splits.values() if v is not None]) == 1:  # If only one dataset is given
+        train, test = data.split(split_ratio = kwargs['split_ratio'], stratified = True, strata_field = kwargs['label'])
         loaded = (train, None, test)
 
-    return loaded
+    data_field.build_vocab()
+    label_field.build_vocab()
+
+    train, dev, test = data.generate_batches(sort_func, loaded, batch_sizes)
+    train_batch = BatchGenerator(train, 'data', 'label')
+    dev_batch = BatchGenerator(dev, 'data', 'label') if dev is not None else None
+    test_batch = BatchGenerator(test, 'data', 'label')
+
+    batches = (train_batch, dev_batch, test_batch)
+    return data, batches
 
 
 def setup_data():
@@ -63,14 +105,14 @@ def setup_data():
     mftc.fields = fields
 
     mftc_data, _, _ = mftc.load_data()
-    mftc_train, mftc_test = mftc.split(split_ration = 0.8, stratified = True, strata_field = 'label')
+    mftc_train, mftc_test = mftc.split(split_ratio = 0.8, stratified = True, strata_field = 'label')
     mftc_text.build_vocab(mftc_train)
     mftc_label.build_vocab(mftc_train)
 
     train_iter, test_iter = mftc.generate_batches(sort_func, (mftc_train, mftc_test))
     mftc_train_batch = BatchGenerator(train_iter, 'data', 'label')
     mftc_test_batch = BatchGenerator(test_iter, 'data', 'label')
-    mftc_loaded = (mftc_train_batch, mftc_test_batch)
+    mftc_loaded = (mftc, mftc_train_batch, mftc_test_batch)
 
     # Sentiment analysis
     sent = Dataset(data_dir = data_dir,
