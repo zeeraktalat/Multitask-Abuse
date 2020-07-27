0a576fa374e1edb0d9896207add59a0b51084264
Zeerak Waseem <zeerakw@laptop>
2020-07-27 20:56:30 +0100

    Update argparse to use jsonargparse and update arguments accordingly.

--- experiments/experiment_runner.py
+++ experiments/experiment_runner.py
@@ -13,43 +13,51 @@ from mlearn.utils.pipeline import process_and_batch, hyperparam_space
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "Run Experiments to generalise models.")
+    parser = ArgumentParser(description = "Run Experiments using MTL.")
 
-    # For all models
-    parser.add_argument('--gpu', help = "Set to run on GPU", action = 'store_true', default = False)
-    parser.add_argument('--seed', help = "Set the random seed.", type = int, default = 32)
-    parser.add_argument("--train", help = "Choose train data: davidson, Waseem, Waseem and Hovy, wulczyn, and garcia.")
-    parser.add_argument("--model", help = "Choose the model to be run: CNN, RNN, LSTM, MLP, LR.", default = "mlp")
-    parser.add_argument("--metrics", help = "Set the metrics to be used.", nargs = '+', default = ["f1"], type = str)
-    parser.add_argument("--display", help = "Metric to display in TQDM loops.")
-    parser.add_argument("--results", help = "Set file to output results to.")
-    parser.add_argument("--batch_size", help = "Set the batch size.", default = 64, type = int)
-    parser.add_argument("--save_model", help = "Directory to store models in.")
+    parser.add_argument("--main", help = "Choose train data: Davidson, Waseem, Waseem and Hovy, Wulczyn, and Garcia.",
+                        type = str.lower, default = 'Davidson')
+    parser.add_argument("--model", help = "Choose the model to be run: CNN, RNN, LSTM, MLP, LR.", nargs = '+',
+                        default = ['mlp'], type = str.lower)
+    parser.add_argument("--save_model", help = "Directory to store models in.", default = 'results/models/')
+    parser.add_argument("--results", help = "Set file to output results to.", default = 'results/')
+    parser.add_argument("--cleaners", help = "Set the cleaning routines to be used.", nargs = '+', default = None)
+    parser.add_argument("--metrics", help = "Set the metrics to be used.", nargs = '+', default = ["f1"],
+                        type = str.lower)
+    parser.add_argument("--stop_metric", help = "Set the metric to be used for early stopping", default = "loss")
+    parser.add_argument("--patience", help = "Set the number of epochs to keep trying to find a new best",
+                        default = None, type = int)
+    parser.add_argument("--display", help = "Metric to display in TQDM loops.", default = 'f1-score')
+    parser.add_argument("--datadir", help = "Path to the datasets.", default = 'data/')
+
+    # Model architecture
+    parser.add_argument("--embedding_dim", help = "Set the embedding dimension.", default = [300], type = int,
+                        nargs = '+')
+    parser.add_argument("--hidden_dim", help = "Set the hidden dimension.", default = [128], type = int, nargs = '+')
+    parser.add_argument("--layers", help = "Set the number of layers.", default = 1, type = int)
+    parser.add_argument("--optimizer", help = "Optimizer to use.", default = 'adam', type = str.lower)
+    parser.add_argument("--loss", help = "Loss to use.", default = 'nlll', type = str.lower)
+    parser.add_argument('--encoding', help = "Select encoding to be used: Onehot, Embedding, Tfidf, Count",
+                        default = 'embedding', type = str.lower)
 
     # Model (hyper) parameters
-    parser.add_argument("--loss", help = "Loss to use.", default = 'NLLL')
-    parser.add_argument("--epochs", help = "Set the number of epochs.", default = 200, type = int)
-    parser.add_argument("--dropout", help = "Set value for dropout.", default = 0.0, type = float)
-    parser.add_argument("--optimizer", help = "Optimizer to use.", default = 'adam')
-    parser.add_argument("--learning_rate", help = "Set the learning rate for the model.", default = 0.01, type = float)
-
-    # All
-    parser.add_argument('--onehot', help = "Use one-hot tensors.", action = 'store_true', default = False)
-    parser.add_argument('--shuffle', help = "Shuffle dataset between epochs", action = 'store_true', default = True)
-
-    # RNN / LSTM
-    parser.add_argument("--layers", help = "Set the number of layers.", default = 1, type = int)
-    parser.add_argument("--hidden", help = "Set the hidden dimension.", default = [128, 128], type = int, nargs = '+')
-    parser.add_argument("--embedding", help = "Set the embedding dimension.", default = 300, type = int)
-
-    # CNN
-    parser.add_argument("--filters", help = "Set the number of filters for CNN.", default = 128, type = int)
-    parser.add_argument("--max_feats", help = "Set the number of features for CNN.", default = 100, type = int)
-    parser.add_argument("--window_sizes", help = "Set the window sizes.", nargs = '+', default = [2, 3, 4], type = int)
+    parser.add_argument("--epochs", help = "Set the number of epochs.", default = [200], type = int, nargs = '+')
+    parser.add_argument("--batch_size", help = "Set the batch size.", default = [64], type = int, nargs = '+')
+    parser.add_argument("--dropout", help = "Set value for dropout.", default = [0.0], type = float, nargs = '+')
+    parser.add_argument('--learning_rate', help = "Set the learning rate for the model.", default = [0.01],
+                        type = float, nargs = '+')
+    parser.add_argument("--nonlinearity", help = "Set nonlinearity function for neural nets.", default = ['tanh'],
+                        type = str.lower, nargs = '+')
+    parser.add_argument("--hyperparams", help = "List of names of the hyper parameters to be searched.",
+                        default = ['epochs'], type = str.lower, nargs = '+')
 
     # Experiment parameters
-    parser.add_argument("--cleaners", help = "Set the cleaning routines to be used.", nargs = '+', default = None)
-    parser.add_argument("--features", help = "Decide which features to train on.", default = "word_token")
+    parser.add_argument('--shuffle', help = "Shuffle dataset between epochs", type = bool, default = True)
+    parser.add_argument('--gpu', help = "Set to run on GPU", type = bool, default = False)
+    parser.add_argument('--seed', help = "Set the random seed.", type = int, default = 32)
+    parser.add_argument("--slur_window", help = "Set window size for slur replacement.", default = None, type = int,
+                        nargs = '+')
+    parser.add_argument('--cfg', action = ActionConfigFile, default = None)
 
     args = parser.parse_args()
 
