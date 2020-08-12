e4feb048c16016a41ee96d64fc544df1421e57db
Zeerak Waseem <zeerakw@laptop>
2020-08-12 23:36:38 +0100

    Add davidson embedding trial

--- experiments/experiment_runner.py
+++ experiments/experiment_runner.py
@@ -134,8 +134,9 @@ if __name__ == "__main__":
 
         aux = [loaders.wulczyn(c, args.datadir, preprocessor = experiment,
                                stratify = 'label', skip_header = True),
-               waseem, loaders.preotiuc_user(c, args.datadir, preprocessor = experiment, label_processor = None,
-                                             stratify = 'label'),
+               waseem,
+               # loaders.preotiuc_user(c, args.datadir, preprocessor = experiment, label_processor = None,
+               #                       stratify = 'label'),
                loaders.oraby_sarcasm(c, args.datadir, preprocessor = experiment, stratify = 'label'),
                loaders.oraby_fact_feel(c, args.datadir, preprocessor = experiment),
                loaders.hoover(c, args.datadir, preprocessor = experiment, stratify = 'label')
