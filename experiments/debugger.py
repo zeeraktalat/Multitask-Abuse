74d0ade36e975ef9358f58cf578e4d705fb2aded
Zeerak <z.w.butt@sheffield.ac.uk>
2020-12-18 04:32:01 +0000

    Add debuggers

--- experiments/debugger.py
+++ experiments/debugger.py
@@ -4,83 +4,86 @@ import numpy as np
 from mlearn import base
 from tqdm import tqdm, trange
 from mlearn.utils.metrics import Metrics
-from mlearn.modeling.embedding import MLPClassifier
 from mlearn.data.clean import Cleaner, Preprocessors
+from mlearn.utils.train import train_singletask_model
 from jsonargparse import ArgumentParser, ActionConfigFile
 from mlearn.utils.train import run_singletask_model as run_model
-from mlearn.utils.train import train_singletask_model
+from mlearn.modeling.embedding import MLPClassifier, CNNClassifier
 from mlearn.utils.pipeline import process_and_batch, param_selection
+from mlearn.data.batching import TorchtextExtractor
 from torchtext.data import TabularDataset, Field, LabelField, BucketIterator
 
-class TorchTextDefaultExtractor:
-    """A class to get index-tensor batches from torchtext data object."""
-
-    def __init__(self, datafield: str, labelfield: str, dataloader: base.DataType):
-       """Initialize batch generator for torchtext."""
-       self.data, self.df, self.lf = dataloader, datafield, labelfield
-
-    def __len__(self):
-       """Get length of the batches."""
-       return len(self.data)
-
-    def __iter__(self):
-       """Iterate over batches in the data."""
-       for batch in self.data:
-          X = getattr(batch, self.df)
-          y = getattr(batch, self.lf)
-          yield (X, y)
-
 # Initialize experiment
-datadir = 'data/'
-torch.random.manual_seed(42)
+datadir =  'data/json'
+torch.random.manual_seed( 42)
 np.random.seed(42)
-encoding = 'index'
-tokenizer = 'bpe'
-metrics = ['f1-score', 'precision', 'recall', 'accuracy']
+encoding =  'index'
+tokenizer = 'ekphrasis'
+metrics =  ['f1-score', 'precision', 'recall', 'accuracy']
+cleaners = ['username', 'url', 'hashtag', 'lower']
 display_metric = stop_metric = 'f1-score'
-batch_size=  64
-epochs = 50
-learning_rate = 0.001
-dropout = 0.0
-embedding = 100
-hidden = 100
-nonlinearity = 'relu'
-gpu = False
+batch_size =  64
+epochs =  200
+learning_rate =  0.01
+dropout =  0.1
+embedding =  200
+hidden =  200
+nonlinearity =  'relu'
+filters =  128
+window_sizes =  [2,3,4]
+gpu = True
 hyperopt = False
 save_path = None
 train_metrics = Metrics(metrics, display_metric, stop_metric)
 dev_metrics = Metrics(metrics, display_metric, stop_metric)
+exp = 'liwc'
 
-c = Cleaner(['url', 'hashtag', 'username', 'lower'])
-experiment = Preprocessors(datadir).select_experiment('word')
+c = Cleaner(cleaners) # Cleaner(['url', 'hashtag', 'username', 'lower'])
+experiment = Preprocessors('data/').select_experiment(exp)
 onehot = True if encoding == 'onehot' else False
 
 if tokenizer == 'spacy':
-    tokenizer = c.tokenize
+    selected_tok  = c.tokenize
 elif tokenizer == 'bpe':
-    tokenizer = c.bpe_tokenize
-elif tokenizer == 'ekphrasis':
-    tokenizer = c.ekphrasis_tokenize
+    selected_tok = c.bpe_tokenize
+elif tokenizer == 'ekphrasis' and exp == 'word':
+    selected_tok = c.ekphrasis_tokenize
     annotate = {'elongated', 'emphasis'}
-    filters = [f"<{filtr}>" for filtr in annotate]
-    c._load_ekphrasis(annotate, filters)
+    flters = [f"<{filtr}>" for filtr in annotate]
+    c._load_ekphrasis(annotate, flters)
+elif tokenizer == 'ekphrasis' and exp == 'liwc':
+    ekphr = c.ekphrasis_tokenize
+    annotate = {'elongated', 'emphasis'}
+    flters = [f"<{filtr}>" for filtr in annotate]
+    c._load_ekphrasis(annotate, flters)
+    def liwc_toks(doc):
+        tokens = ekphr(doc)
+        tokens = experiment(tokens)
+        return tokens
+    selected_tok = liwc_toks
+tokenizer = selected_tok
 
 text = Field(tokenize = tokenizer, lower = True, batch_first = True)
 label = LabelField()
-fields = [('ignore', None), ('text', text), ('label', label), ('ignore', None)]
-train, dev, test = TabularDataset.splits('/home/zeerakw/projects/MTL/data/', train = 'wulczyn_train.tsv',
-                                         validation = 'wulczyn_dev.tsv', test = 'wulczyn_test.tsv', 
-                                         format = 'tsv', skip_header = True, fields = fields)
+fields = {'text': ('text', text), 'label': ('label', label)}  # Because we load from json we just need this.
+train, dev, test = TabularDataset.splits(datadir, train = 'davidson_binary_train.json',
+                                         validation = 'davidson_binary_dev.json', test = 'davidson_binary_test.json', 
+                                         format = 'json', skip_header = True, fields = fields)
 text.build_vocab(train)
 label.build_vocab(train)
 
-model = MLPClassifier(len(text.vocab.stoi), embedding, hidden, len(label.vocab.stoi), False, nonlinearity)
-loss = torch.nn.NLLLoss()
-optimizer = torch.optim.Adam(model.parameters(), learning_rate)
+model = MLPClassifier(len(text.vocab.stoi), embedding, hidden, len(label.vocab.stoi), dropout, True, nonlinearity)
+loss = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), learning_rate)
 
 train_ds = BucketIterator(dataset = train, batch_size = batch_size)
 dev_ds = BucketIterator(dataset = dev, batch_size = batch_size)
-batched_train = TorchTextDefaultExtractor('text', 'label', train_ds)
-batched_dev = TorchTextDefaultExtractor('text', 'label', dev_ds)
+batched_train = TorchtextExtractor('text', 'label', 'davidson_binary_train', train_ds)
+batched_dev = TorchtextExtractor('text', 'label', 'davidson_binary_dev', dev_ds)
 
-train_singletask_model(model, save_path, epochs, batched_train, loss, optimizer, train_metrics, dev = batched_dev, dev_metrics = dev_metrics, shuffle = False, gpu = True)
+train_singletask_model(model, save_path, epochs, batched_train, loss, optimizer, train_metrics, dev = batched_dev, dev_metrics = dev_metrics, shuffle = False, gpu = True, clip = 1.0)
+
+max_train = np.argmax(train_metrics.scores['f1-score'])
+max_dev = np.argmax(dev_metrics.scores['f1-score'])
+print(max_train, train_metrics.scores['f1-score'][max_train])
+print(max_dev, dev_metrics.scores['f1-score'][max_dev])
