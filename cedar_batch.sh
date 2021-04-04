aa47f34d7f990252f102231030070551f7c66abd
Zeerak Waseem <zeerak@cedar1.cedar.computecanada.ca>
2021-04-03 19:42:45 -0700

    Bugfix torchtext runner for cedar

--- cedar_batch.sh
+++ cedar_batch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #SBATCH --gres=gpu:p100l:4      # request GPU "generic resource"
-#SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=16000M       # memory per node
+#SBATCH --cpus-per-task=2   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=6000M       # memory per node
 #SBATCH --time=0-00:20   # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 
+module load StdEnv/2020
 module load cuda
 module load cudnn/8.0.3
 
-source /home/zeerak/.venvs/mtl/bin/activate
+source /home/zeerak/.venv/mtl/bin/activate
 
-parallel --joblog parallel.log < /scratch/zeerak/MTL/configs/sweep_commands.txt
+#wandb agent zeerak/MTL_test/0glnot3x --count 5
+# parallel --joblog parallel.log < /scratch/zeerak/MTL/configs/sweep_commands.txt
+wandb agent zeerak/MTL_test/asr8dh5p
