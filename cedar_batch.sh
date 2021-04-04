1026de3155f3b0493b99c9c3f597b3a016269146
Zeerak Waseem <zeerak@cedar1.cedar.computecanada.ca>
2021-04-03 21:46:50 -0700

    Try with parallels and move logs to logdir

--- cedar_batch.sh
+++ cedar_batch.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=2   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=6000M       # memory per node
 #SBATCH --time=0-00:20   # time (DD-HH:MM)
-#SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+#SBATCH --output=/scratch/zeerak/MTL/logs/%N-%j.out  # %N for node name, %j for jobID
 
 module load StdEnv/2020
 module load cuda
@@ -12,5 +12,5 @@ module load cudnn/8.0.3
 source /home/zeerak/.venv/mtl/bin/activate
 
 #wandb agent zeerak/MTL_test/0glnot3x --count 5
-# parallel --joblog parallel.log < /scratch/zeerak/MTL/configs/sweep_commands.txt
-wandb agent zeerak/MTL_test/asr8dh5p
+parallel --joblog /scratch/zeerak/MTL/logs/parallel.log < /scratch/zeerak/MTL/configs/sweep_commands.txt
+# wandb agent zeerak/MTL_test/asr8dh5p
