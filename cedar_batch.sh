d269c441473bb38259f1f3216a08aab642de5ecb
Zeerak Waseem <zeerak@cedar1.cedar.computecanada.ca>
2021-04-20 12:18:22 -0700

    Update tiem required

--- cedar_batch.sh
+++ cedar_batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --gres=gpu:p100l:4      # request GPU "generic resource"
-#SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=100000M       # memory per node
-#SBATCH --time=00-00:20   # time (DD-HH:MM)
+#SBATCH --gres=gpu:p100l:4 # request GPU "generic resource"
+#SBATCH --cpus-per-task=4  # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
+#SBATCH --mem=100000M      # memory per node
+#SBATCH --time=00-24:20    # time (DD-HH:MM)
 #SBATCH --output=/scratch/zeerak/MTL/logs/%N-%j.out  # %N for node name, %j for jobID
 
 module load StdEnv/2020
